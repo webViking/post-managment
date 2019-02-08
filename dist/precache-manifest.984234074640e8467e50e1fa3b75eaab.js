f76a7cda6844e18170b251e1230575b5181f6f6c
Dawid.Rym <dawid.jscript@gmail.com>
2019-02-08 19:23:28 +0100

    Deployed project

---
+++ dist/precache-manifest.984234074640e8467e50e1fa3b75eaab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "918689267baed3bc25eb",
+    "url": "/static/js/main.91868926.chunk.js"
+  },
+  {
+    "revision": "f140fd0eb9d8d23112f8",
+    "url": "/static/js/1.f140fd0e.chunk.js"
+  },
+  {
+    "revision": "918689267baed3bc25eb",
+    "url": "/static/css/main.5c83df3b.chunk.css"
+  },
+  {
+    "revision": "22d20ec4b74b5f82bd984c7ac1ffd7fe",
+    "url": "/index.html"
+  }
+];
